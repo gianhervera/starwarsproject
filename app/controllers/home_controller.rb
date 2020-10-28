0be882f8a00cd666ebee0d16f110ce104abc50c5
ghervera <59581339+ghervera@users.noreply.github.com>
2020-10-28 12:11:54 -0500

    Added about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @page = params.fetch(:page, 0).to_i
+    @planets = Planet.offset(@page * PLANETS_PER_PAGE).limit(PLANETS_PER_PAGE)
   end
 end
